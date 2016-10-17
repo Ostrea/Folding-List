74b3a750e8c764cd147ba0d82167b6596ab3a48e
Ostrea <Ostrea@users.noreply.github.com>
2016-10-17 18:26:03 +0300

    Add app to installed.

--- test_assignment/settings.py
+++ test_assignment/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test_assignment_app.apps.TestAssignmentAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
