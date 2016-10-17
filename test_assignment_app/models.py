cf9a73b88244215a27030678a77fc2bf2cba3f8c
Ostrea <Ostrea@users.noreply.github.com>
2016-10-17 19:53:39 +0300

    Basic models and printing.

--- test_assignment_app/models.py
+++ test_assignment_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu:
+    # name = ''
+    # items = []
+
+    def __init__(self, name, items=None):
+        self.name = name
+        self.items = items
+
+
+class MenuItem:
+    # value = ''
+    # sub_items = []
+
+    def __init__(self, value):
+        self.value = value
--- test_assignment_app/templatetags/test_assignment_app_tags.py
+++ test_assignment_app/templatetags/test_assignment_app_tags.py
@@ -1,10 +1,30 @@
 from django import template
 
+from ..models import Menu, MenuItem
+
 
 register = template.Library()
 
 
 @register.inclusion_tag('test_assignment_app/menu.html')
 def draw_menu(menu_name):
-    choices = ['one', 'two', 'three']
-    return {'choices': choices}
+    menu = Menu('Super menu', [MenuItem('First'), MenuItem('Second'),
+                               Menu('Third',
+                                    [MenuItem('Sub third one'),
+                                     Menu('Sub third two',
+                                          [MenuItem('Sub sub third two')])])])
+    return {'items': nested_to_flat(menu)}
+
+
+def nested_to_flat(menu):
+    if isinstance(menu, Menu):
+        yield {'data': menu.name, 'is_data': True}
+        yield {'start_nodes': True}
+        for node in menu.items:
+            yield {'start_node': True}
+            for i in nested_to_flat(node):
+                yield i
+            yield {'end_node': True}
+        yield {'end_nodes': True}
+    else:
+        yield {'data': menu.value, 'is_data': True}
