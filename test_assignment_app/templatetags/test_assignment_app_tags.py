777882bbbdbf4113f2fa3f312ffbba1c54ef92c6
Ostrea <Ostrea@users.noreply.github.com>
2016-10-17 18:53:50 +0300

    Add template tag for drawing menu.
    Just placeholder for now.

--- test_assignment_app/templatetags/test_assignment_app_tags.py
+++ test_assignment_app/templatetags/test_assignment_app_tags.py
@@ -2,3 +2,9 @@ from django import template
 
 
 register = template.Library()
+
+
+@register.inclusion_tag('test_assignment_app/menu.html')
+def draw_menu(menu_name):
+    choices = ['one', 'two', 'three']
+    return {'choices': choices}
