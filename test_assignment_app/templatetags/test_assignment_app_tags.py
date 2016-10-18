fbddaad22faad2ec06364d8912047587fbd62cf0
Ostrea <Ostrea@users.noreply.github.com>
2016-10-18 13:54:15 +0300

    Pass menu name and flag if its highest level menu.

--- test_assignment_app/templatetags/test_assignment_app_tags.py
+++ test_assignment_app/templatetags/test_assignment_app_tags.py
@@ -9,17 +9,18 @@ register = template.Library()
 @register.inclusion_tag('test_assignment_app/menu.html')
 def draw_menu(menu_name):
     menu = Menu.objects.get(name=menu_name)
-    return {'items': nested_to_flat(menu)}
+    return {'items': nested_to_flat(menu, True)}
 
 
-def nested_to_flat(node):
+def nested_to_flat(node, highest_level_menu):
     if node.children.exists():
         yield {'link': node.link, 'data': node.name, 'is_data': True}
-        yield {'start_nodes': True}
+        yield {'start_nodes': True, 'highest_level_menu': highest_level_menu,
+               'menu_name': node.name.lower().replace(' ', '_')}
 
         for menu_item in node.children.all():
             yield {'start_node': True}
-            for item in nested_to_flat(menu_item):
+            for item in nested_to_flat(menu_item, False):
                 yield item
             yield {'end_node': True}
 
