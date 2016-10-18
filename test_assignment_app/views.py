f2fc9ecf9a49be0ee875033c012bf1af6de8232a
Ostrea <Ostrea@users.noreply.github.com>
2016-10-18 15:17:01 +0300

    Handle query parameters.

--- test_assignment_app/views.py
+++ test_assignment_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
-    return render(request, 'test_assignment_app/index.html')
+    menu_name = request.GET.get('menu_name')
+    active_item = None
+    if menu_name:
+        active_item = request.GET.get('active_item')
+        if not active_item:
+            raise Http404
+
+    return render(request, 'test_assignment_app/index.html', {
+        'request_menu_name': menu_name,
+        'active_item': active_item
+    })
